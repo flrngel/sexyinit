40d4176e4f244d329cb72a8613662e599cf16593
flrngel <flrngel@gmail.com>
2013-07-03 18:12:45 +0900

    javascript

--- install.sh
+++ install.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+
+# git settings
+
 cp -rf .gitconfig ~/
+
+# bash settings
 cp -rf .bash* ~/
+
+
+# vim settings 
 cp -rf .vimrc ~/
 mkdir -p ~/.vim/autoload ~/.vim/bundle; \
 curl -Sso ~/.vim/autoload/pathogen.vim \
     https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
 cd ~/.vim/bundle
 git clone git://github.com/tpope/vim-rails.git
 git clone git://github.com/tpope/vim-bundler.git
-
+git clone https://github.com/pangloss/vim-javascript.git
