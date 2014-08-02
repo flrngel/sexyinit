e3008b74b8928204632382e3c5074175a5d4417b
Hwechul Cho (Derrick) <flrngel@gmail.com>
2014-08-02 16:50:40 +0900

    ctrlp.vim added

--- install.sh
+++ install.sh
@@ -34,5 +34,6 @@ git clone https://github.com/kchmck/vim-coffee-script.git
 git clone https://github.com/groenewege/vim-less.git
 git clone https://github.com/junegunn/seoul256.vim.git
 git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/kien/ctrlp.vim.git
 
 echo "execute command: \"source ~/.bash_profile\""
